503e4d95f08c9aaffeb1e30df0533ad7b5c94a96
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2023-03-17 23:51:47 +0000

    V 0.1
    Criando jogo com Python

--- guessgame.py
+++ guessgame.py
@@ -5,3 +5,10 @@ secret_number = 42
 guess = input("Digite o seu número: ")
 
 print("Você digitou ", guess)
+
+guess = int(guess)
+
+if(secret_number == guess):
+    print("Você acertou!")
+else:
+    print("Você errou.")

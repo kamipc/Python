967258edfed0c943d64835b6d1996b0f7dae0575
Camila P. Costa <126345439+kamipc@users.noreply.github.com>
2023-03-18 01:25:37 +0000

    V 0.2
    Adicionado um número de tentativas para acertar o número secreto

--- guessgame.py
+++ guessgame.py
@@ -1,14 +1,25 @@
 print("Bem vindo ao jogo de adivinhação!")
 
 secret_number = 42
+tries = 3
+round = 1
+while(round <= tries):
+    guess = input("Tente advinhar um número: ")
+    #input sempre volta o resultado como string
 
-guess = input("Digite o seu número: ")
+    print("Você digitou ", guess)
 
-print("Você digitou ", guess)
+    guess = int(guess)
 
-guess = int(guess)
+    #para adicionar condição no else usa elif
+    if(guess == secret_number):
+        print("Você acertou!")
+    else:
+        if(guess > secret_number):
+            print("Incorreto! Seu número é maior do que o número secreto")
+        else:
+            print("Incorreto! Seu número é menor do que o número secreto")
+    
+    round = round + 1
 
-if(secret_number == guess):
-    print("Você acertou!")
-else:
-    print("Você errou.")
+print("Fim de Jogo.")
